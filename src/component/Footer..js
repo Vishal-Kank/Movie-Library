e6fe5672e5eead5cac9908db92fa42a3a75fdaff
Vishal-Kank <kankvishalkank@gmail.com>
2019-10-23 19:35:06 +0530

    header & footer

---
+++ src/component/Footer..js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <footer>
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="footer p-3 mt-4 text-center bg-dark text-light">
+                            Developed By :<span className="text-warning font-weigth-normal"> Vishal Kank</span>
+                            , Using <i className="fab fa-react" /> React Js & Redux Js integrated with external movies data API
+                            <a href="http://www.omdbapi.com/" target="_blank"> OMDB</a>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        )
+    }
+}
