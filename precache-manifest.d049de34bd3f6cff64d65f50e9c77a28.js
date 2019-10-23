614b85e8bdda52d6607a41d880645404271f15c5
Vishal-Kank <kankvishalkank@gmail.com>
2019-10-23 20:01:39 +0530

    Updates

---
+++ precache-manifest.d049de34bd3f6cff64d65f50e9c77a28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da739204ac70e6c531e7b798bfeb921a",
+    "url": "/Movie-Library/index.html"
+  },
+  {
+    "revision": "7d06774244f282118b65",
+    "url": "/Movie-Library/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "45c0424077756157ec64",
+    "url": "/Movie-Library/static/js/2.a658758c.chunk.js"
+  },
+  {
+    "revision": "7d06774244f282118b65",
+    "url": "/Movie-Library/static/js/main.8cd735ec.chunk.js"
+  },
+  {
+    "revision": "4ace5832fe82ba4d5b98",
+    "url": "/Movie-Library/static/js/runtime-main.fc64de6b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Movie-Library/static/media/logo.25bf045c.svg"
+  }
+]);
