9bc68838c4e510c57ed45d77aa8d5aeb24590798
Vishal-Kank <kankvishalkank@gmail.com>
2019-12-05 19:05:25 +0530

    Updates

--- precache-manifest.abe00de241e6f4ee9f0afdf81a639d9f.js
+++ precache-manifest.abe00de241e6f4ee9f0afdf81a639d9f.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6d3565fddc150d283b99f7cf3f8d1c3d",
+    "revision": "47e7299c4a53010a471c1b5d3c22418f",
     "url": "/Movie-Library/index.html"
   },
   {
-    "revision": "7bcae4cd2294adb8a1fe",
+    "revision": "513fb6d7ab396ea0d49c",
     "url": "/Movie-Library/static/css/main.09eb4ed7.chunk.css"
   },
   {
-    "revision": "2edd82cf6bfc46b1bf76",
-    "url": "/Movie-Library/static/js/2.fe92eda9.chunk.js"
+    "revision": "7f86a607608875c28e0f",
+    "url": "/Movie-Library/static/js/2.3e3b180f.chunk.js"
   },
   {
-    "revision": "7bcae4cd2294adb8a1fe",
-    "url": "/Movie-Library/static/js/main.bb305ffa.chunk.js"
+    "revision": "513fb6d7ab396ea0d49c",
+    "url": "/Movie-Library/static/js/main.fddc19ea.chunk.js"
   },
   {
     "revision": "4ace5832fe82ba4d5b98",
